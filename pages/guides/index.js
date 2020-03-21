306b46d8e42db4d15c7812b495ad0bb8e95b27ef
Joshua Ling <joshling1919@gmail.com>
2020-03-21 14:09:33 -0700

    Route to teacher guide as default

--- pages/guides/index.js
+++ pages/guides/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Nav from '../../components/nav';
 
 const navItems = [
@@ -9,22 +10,31 @@ const navItems = [
   { text: 'For Parents', href: '/guides/for-parents' },
 ];
 
-const Guides = ({ children }) => (
-  <div className="guides-container">
-    <div className="guides-nav-container">
-      <Nav className="guides-nav" navItems={navItems} />
+const Guides = ({ children }) => {
+  const router = useRouter();
+  useEffect(() => {
+    // redirect to teacher guide as default guide:
+    if (router.pathname === '/guides') {
+      router.push('/guides/for-teachers');
+    }
+  }, []);
+  return (
+    <div className="guides-container">
+      <div className="guides-nav-container">
+        <Nav className="guides-nav" navItems={navItems} />
+      </div>
+      <div className="guides-content">{children}</div>
+      <style jsx>{`
+        .guides-nav-container {
+          margin-top: 20px;
+          font-size: 12px;
+        }
+        .guides-content {
+          padding: 20px;
+        }
+      `}</style>
     </div>
-    <div className="guides-content">{children}</div>
-    <style jsx>{`
-      .guides-nav-container {
-        margin-top: 20px;
-        font-size: 12px;
-      }
-      .guides-content {
-        padding: 20px;
-      }
-    `}</style>
-  </div>
-);
+  );
+};
 
 export default Guides;
