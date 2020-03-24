6097244b1209ab70b0df978a53fcf93af81018bb
Joshua Ling <joshling1919@gmail.com>
2020-03-24 00:03:04 -0700

    Edit index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Resources from '../components/resources';
 import Intro from '../components/shared/Intro';
+import Anchor from '../components/shared/Anchor';
 
 const Index = () => (
   <div>
@@ -35,9 +36,38 @@ const Index = () => (
       </p>
       <p>
         The vast amount of available resources is both encouraging and somewhat
-        intimidating. Our goal here is to simplify things by sharing what we
-        think are the best tools to start with.
+        intimidating. We want to simplify things by sharing what we think are
+        the best tools to start with.
       </p>
+      <p>
+        If you're looking for somewhere to start, Khan Academy and Google have
+        both provided excellent, comprehensive guides for how to teach remotely:
+      </p>
+      <ul>
+        <li>
+          <Anchor
+            text="Khan Academy's response to school closures"
+            href="https://khanacademy.zendesk.com/hc/en-us/articles/360040167432-How-can-Khan-Academy-be-used-for-remote-learning-during-school-closures-"
+          />
+        </li>
+        <li>
+          <Anchor
+            text="Google's Teach From Home guide"
+            href="https://teachfromhome.google/"
+          />
+        </li>
+      </ul>
+
+      <p>
+        Several companies are also holding webinars on how to use their tools to
+        facilitate and enhance distance learning.{' '}
+        <Anchor
+          href="https://docs.google.com/spreadsheets/d/1DCWiz6gFzG-wODlCkNs_en1NF9N_1wGwpRrywhhGklQ/edit?usp=sharing"
+          text="Stay up to date with those webinars here."
+        />
+      </p>
+
+      <p>Finally, please check out the curated list of resources below.</p>
     </Intro>
     <Resources />
 
